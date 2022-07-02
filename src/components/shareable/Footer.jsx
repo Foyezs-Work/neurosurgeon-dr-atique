c67ffc36e34484415410d3bf062b72171dcdd636
ma-foyez <mafoyez.web@gmail.com>
2022-07-02 12:26:15 +0600

    footer issue fixed

--- src/components/shareable/Footer.jsx
+++ src/components/shareable/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
                                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.2749853445735!2d90.40749491449282!3d23.737571495184277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8f4de15d5eb%3A0x35df10409c6ffeef!2sIslami%20Bank%20Central%20Hospital%2C%20Kakrail!5e0!3m2!1sen!2sbd!4v1655841357410!5m2!1sen!2sbd" className='border shadow' title="Neurosurgeon dr. atique" width="100%" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                 </div>
                             </div>
-                            <div className="col-xl-3 col-lg-3 col-md-6">
+                            <div className="col-xl-3 col-lg-3 col-md-6 text-end">
                                 <div className="widget widget_form ms-3">
 
                                     <h3 className="footer-title">Site Map</h3>
