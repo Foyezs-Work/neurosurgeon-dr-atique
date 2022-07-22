e64394cd7a348dfea6d0637a11c081688818277e
ma-foyez <mafoyez.bd@gmail.com>
2022-07-22 10:28:06 +0600

    image gallery issue correciton

--- src/pages/PhotosGallery.jsx
+++ src/pages/PhotosGallery.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react';
 import MainLayout from '../layout/MainLayout';
-import mainBanner from "./../assets/images/main-banner/bg1.jpg"
+import mainBanner from "./../assets/images/banner/img1.jpg"
 import { useDispatch, useSelector } from 'react-redux';
 import { handleNavbarPosition } from '../_globalRedux/Action/GlobalAction';
 import trangleOrange from "./../assets/images/shap/trangle-orange.png"
-import squareBlue from "./../assets/images/shap/square-blue.png"
 import circleBlue from "./../assets/images/shap/chicle-blue-2.png"
 import waveOrange from "./../assets/images/shap/wave-orange.png"
 import { getPhotoGalleryList } from '../adminPanel/pages/photoGallery/_redux/Action/PhotoGalleryAction';
 import { galleryPageData } from '../assets/data/galleryData';
-import { ImageViewer } from "react-image-viewer-dv"
+// import { ImageViewer } from "react-image-viewer-dv"
 
 const PhotosGallery = () => {
 
@@ -28,40 +27,47 @@ const PhotosGallery = () => {
 
 
     return (
-        <MainLayout title="Photo Gallery" isLoading={isLoading} loadingMessage="Gallery" >
-            <div className="main-banner"
-                style={{ backgroundImage: `url(${mainBanner})` }}
-            >
-                <div className="container inner-content">
-                    <div className="row align-items-center">
-                        <div className="col-lg-12 col-md-12 col-sm-12 py-5">
-                            <h2 className="title text-center">Photo Gallery</h2>
-                            <div className="row custom_section_padding">
-
-                                {
-                                    galleryPageData.length > 0 && galleryPageData.map((item, index) => (
-                                        <div className='col-md-3 p-3'>
-                                            <div className="img_card">
-                                                <ImageViewer key={index + 1}>
-                                                    <img src={item.url} alt={item.title} className="img-fluid" />
-                                                </ImageViewer>
-                                            </div>
-                                        </div>
-                                    ))
-                                }
+        <MainLayout title="Photo Gallery" isLoading={false} loadingMessage="Gallery">
+            <div className="page-content bg-white">
+                <div class="banner-wraper">
+                    <div class="page-banner"
+                        style={{ backgroundImage: `url(${mainBanner})` }}
+                    >
+                        <div class="container">
+                            <div class="page-banner-entry text-center">
+                                <h1>Photo Gallery</h1>
                             </div>
+                        </div>
 
+
+                        <img class="pt-img1 animate-wave" src={trangleOrange} alt="" />
+                        <img class="pt-img2 animate2" src={circleBlue} alt="" />
+                        <img class="pt-img3 animate-rotate" src={waveOrange} alt="" />
+                    </div>
+                </div>
+
+
+                <div className="section-area section-sp4">
+                    <div className="container">
+                        <div class="row">
+                            {
+                                galleryPageData.length > 0 && galleryPageData.map((item, index) => (
+                                    <div className='col-md-3 p-3'>
+                                        <div className="img_card">
+                                            {/* <ImageViewer key={index + 1}>
+                                                    <img src={item.url} alt={item.title} className="" />
+                                                </ImageViewer> */}
+                                            <img src={item.url} alt={item.title} className="" />
+                                        </div>
+                                    </div>
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
 
-                <img className="pt-img3 animate3" src={circleBlue} alt="" />
-                <img className="pt-img1 animate1" src={trangleOrange} alt="" />
-                <img className="pt-img2 animate2" src={squareBlue} alt="" />
-                <img className="pt-img3 animate3" src={circleBlue} alt="" />
-                <img className="pt-img5 animate-wave" src={waveOrange} alt="" />
             </div>
-        </MainLayout >
+        </MainLayout>
     );
 };
 
